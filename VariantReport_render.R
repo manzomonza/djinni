e12b7191eb7bff09e6ccb30edd953b860a287318
manzomonza <massimiliano.manzo@posteo.ch>
2023-08-28 15:30:43 +0200

    Update Rmarkdown and Render file to improve testing on server

--- VariantReport_render.R
+++ VariantReport_render.R
@@ -4,16 +4,9 @@
 ### OPTPARSE ###
 ################ library(optparse)
 
-snv = readr::read_tsv(parsed_fp$parsed_snv)
-##
-snv = "./testfiles/Y537_B2023.6661_OcaV3_output/parsed_output/parsed_snv.tsv"
-snv = readr::read_tsv(snv)
-snv = VariantAnnotationModules::amino_acid_code_3_to_1(snv)
-
-clinvar_hits = readr::read_tsv('./testfiles/Y537_B2023.6661_OcaV3_output/annotation_output/annotation_clinvar.tsv')
-
-#html_dir = analysis_output_dir(vcfpath)
+html_dir = analysis_output_dir(vcfpath)
 Variant_report_markdown = "/home/ionadmin/github_app/genie/VariantReport_renderN.Rmd"
 rmarkdown::render(Variant_report_markdown,
-                  params = list(snv = snv),
+                  params = list(parsed_fp = parsed_fp,
+                                annotation_fp = annotation_fp),
                   output_file = paste0(html_dir, '/VariantReport.html'))
